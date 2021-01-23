4fe31c19dcc57c4b4f071e82663d53afb5d613f9
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-23 20:12:05 -0300

    commit final

---
+++ 01-Iniciante/URI_1044/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace URI_1044
+{
+    class Program
+    {
+        /*
+        Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", 
+        indicando se os valores lidos são múltiplos entre si.
+        Entrada: A entrada contém valores inteiros.
+        Saída: A saída deve conter uma das mensagens conforme descrito acima.
+        Exemplo de Entrada 	Exemplo de Saída
+        6 24                Sao Multiplos
+	
+        6 25                Nao sao Multiplos 
+         */
+
+        static void Main( string[] args )
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            int a, b;
+            a = int.Parse(s[0]);
+            b = int.Parse(s[1]);
+            if (b % a == 0 || a % b == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
