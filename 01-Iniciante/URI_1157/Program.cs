34d860171d027cc8660f4453bba17844589a7c35
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-23 23:45:31 -0300

    commit final

---
+++ 01-Iniciante/URI_1157/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace URI_1157
+{
+    class Program
+    {
+        /*
+        Ler um número inteiro N e calcular todos os seus divisores.
+        Entrada: O arquivo de entrada contém um valor inteiro.
+        Saída: Escreva todos os divisores positivos de N, um valor por linha.
+        Exemplo de Entrada 	Exemplo de Saída
+        6                   1
+                            2
+                            3
+                            6
+         */
+
+        static void Main( string[] args )
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (n < 0)
+                {
+                    n--;
+                }
+                else if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
