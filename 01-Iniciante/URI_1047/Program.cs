3aa0e2f7096aa3a8c13caa2cfc47152ac6d28a16
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-23 22:38:16 -0300

    add comentários

--- 01-Iniciante/URI_1047/Program.cs
+++ 01-Iniciante/URI_1047/Program.cs
@@ -4,6 +4,17 @@ namespace URI_1047
 {
     class Program
     {
+        /*
+        Leia a hora inicial, minuto inicial, hora final e minuto final de um jogo. A seguir calcule a duração do jogo.
+        Obs: O jogo tem duração mínima de um (1) minuto e duração máxima de 24 horas.
+        Entrada: Quatro números inteiros representando a hora de início e fim do jogo.
+        Saída: Mostre a seguinte mensagem: “O JOGO DUROU XXX HORA(S) E YYY MINUTO(S)” .
+        Exemplo de Entrada 	Exemplo de Saída
+        7 8 9 10            O JOGO DUROU 2 HORA(S) E 2 MINUTO(S)
+	    
+        7 7 7 7             O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)
+         */
+        // erro de compilação. Ide ok.
         static void Main( string[] args )
         {
             string[] s = Console.ReadLine().Split(' ');
@@ -13,7 +24,6 @@ namespace URI_1047
             minutoInicio = int.Parse(s[1]);
             horaFim = int.Parse(s[2]);
             minutoFim = int.Parse(s[3]);
-
 
             if (horaInicio >= horaFim)
             {
@@ -26,7 +36,6 @@ namespace URI_1047
                 {
                     minutosTotais = minutoFim - minutoInicio;
                 }
-                //Console.WriteLine($"O JOGO DUROU {horasTotais} HORA(S) E {minutosTotais} MINUTO(S)");
             }
             else if (horaInicio < horaFim && minutoInicio > minutoFim)
             {
@@ -46,7 +55,6 @@ namespace URI_1047
             {
                 horasTotais = horaFim - horaInicio;
                 minutosTotais = minutoFim - minutoInicio;
-                //Console.WriteLine($"O JOGO DUROU {horasTotais} HORA(S) E {minutosTotais} MINUTO(S)");
             }
             Console.WriteLine($"O JOGO DUROU {horasTotais} HORA(S) E {minutosTotais} MINUTO(S)");
         }
