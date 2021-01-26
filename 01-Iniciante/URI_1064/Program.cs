4edfbb49176e89c1ce341f70c6b870d75a78a79f
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-25 22:34:46 -0300

    commit final

--- 01-Iniciante/URI_1064/Program.cs
+++ 01-Iniciante/URI_1064/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace URI_1064
 {
@@ -20,7 +21,22 @@ namespace URI_1064
 
         static void Main( string[] args )
         {
-            
+            double[] array = new double[6];
+            double soma = 0.0;
+            int cont = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (array[i] > 0)
+                {
+                    cont++;
+                    soma += array[i];
+                }
+            }
+            Console.WriteLine($"{cont} valores positivos");
+            soma /= cont;
+            Console.WriteLine(soma.ToString("F1", CultureInfo.InvariantCulture));
         }
     }
 }
