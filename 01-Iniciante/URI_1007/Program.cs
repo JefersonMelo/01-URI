ecbf34850cc0cf4c86b82082ea77cf5cafbe7976
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-27 15:23:39 -0300

    Update Program.cs
    commit final

--- 01-Iniciante/URI_1007/Program.cs
+++ 01-Iniciante/URI_1007/Program.cs
@@ -5,7 +5,8 @@ namespace URI_1007
     class Program
     {
         /*
-        Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+        Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a 
+	fórmula: DIFERENCA = (A * B - C * D).
         Entrada: O arquivo de entrada contém 4 valores inteiros.
         Saída: Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade.
         Exemplos de Entrada 	Exemplos de Saída
