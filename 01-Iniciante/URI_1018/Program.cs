db70dd41fe82d88a55481ed86de3196b51c75052
Jeferson Melo <jefsdemelo@gmail.com>
2021-01-27 22:49:28 -0300

    commit final
    correção dos comentários

--- 01-Iniciante/URI_1018/Program.cs
+++ 01-Iniciante/URI_1018/Program.cs
@@ -36,17 +36,17 @@ namespace URI_1018
             Console.WriteLine(v);
 
             Console.WriteLine(v / 100 + " nota(s) de R$ 100,00");//576
-            v %= 100;//576 - 500 = 76
+            v %= 100;//76
             Console.WriteLine(v / 50 + " nota(s) de R$ 50,00");//76
-            v %= 50;//76 - 50 = 26
+            v %= 50;//26
             Console.WriteLine(v / 20 + " nota(s) de R$ 20,00");//26
-            v %= 20;//26 - 20 = 6
+            v %= 20;//6
             Console.WriteLine(v / 10 + " nota(s) de R$ 10,00");//6
-            v %= 10;//6 - 10 = 0
+            v %= 10;//0
             Console.WriteLine(v / 5 + " nota(s) de R$ 5,00");//6
-            v %= 5;//6 - 5 = 1
+            v %= 5;//1
             Console.WriteLine(v / 2 + " nota(s) de R$ 2,00");//1
-            v %= 2;//1 - 2 = 0
+            v %= 2;//0
             Console.WriteLine(v + " nota(s) de R$ 1,00");//1
         }
     }
