b84fdb91432debf6aea48764eb5cf637e84a628e
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-07 15:33:08 -0300

    commit final

--- 01-Iniciante/URI_1073/Program.cs
+++ 01-Iniciante/URI_1073/Program.cs
@@ -19,6 +19,7 @@ namespace URI_1073
         static void Main( string[] args )
         {
             int n = int.Parse(Console.ReadLine());
+
             for (int i = 1; i <= n; i++)
             {
                 if (i % 2 == 0)
