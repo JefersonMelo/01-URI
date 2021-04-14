290a8921b66e02490e563509c208bec1f6065e62
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-14 15:10:11 -0300

    commit final conclusão projeto 1036

--- 01-Iniciante/URI_1036/Program.cs
+++ 01-Iniciante/URI_1036/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-
-namespace URI_1036
+using System.Globalization;
+class URI_1036
 {
-    class Program
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+
+        double a, b, c, delta, x1, x2;
+        string[] s = Console.ReadLine().Split(' ');
+        a = double.Parse(s[0]);
+        b = double.Parse(s[1]);
+        c = double.Parse(s[2]);
+        delta = Math.Pow(b, 2) - 4 * a * c;
+        if (delta >= 0 && a != 0)
         {
-            Console.WriteLine("Hello World!");
+            x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+            Console.WriteLine($"R1 = {x1.ToString("F5", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"R2 = {x2.ToString("F5", CultureInfo.InvariantCulture)}");
         }
+        else
+        {
+            Console.WriteLine("Impossivel calcular");
+        }
+
     }
+
 }
