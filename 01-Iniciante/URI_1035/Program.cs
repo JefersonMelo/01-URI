51e84328f1002617bdab40009434b45dca51459e
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-14 14:51:15 -0300

    commit com solução URI_1035 e readme inicial

---
+++ 01-Iniciante/URI_1035/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace URI_1035
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, c, d;
+            string[] s = Console.ReadLine().Split(' ');
+            a = int.Parse(s[0]);
+            b = int.Parse(s[1]);
+            c = int.Parse(s[2]);
+            d = int.Parse(s[3]);
+            if (b > c && d > a && (c + d) > (a + b) && a % 2 == 0 && c > 0 && d > 0)
+            {
+                Console.WriteLine("Valores aceitos");
+            }
+            else
+            {
+                Console.WriteLine("Valores nao aceitos");
+            }
+
+        }
+    }
+}
