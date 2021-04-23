dcaabaa2021fb7848f639c0064b49f21144dd88c
Jeferson Melo <jefsdemelo@gmail.com>
2021-04-23 13:19:30 -0300

    commit final, solução aceita no uri.

--- 04-Estruturas_e_Bibliotecas/URI_1259/Program.cs
+++ 04-Estruturas_e_Bibliotecas/URI_1259/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace URI_1259
 {
     class Program
     {
+        /*
+         * https://pt.stackoverflow.com/questions/415521/unir-duas-listas-c
+         * 
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            int n = int.Parse(Console.ReadLine());
+            int[] vet = new int[n];
+
+            List<int> listPar = new List<int>();
+            List<int> listImpar = new List<int>();
+
+            for ( int i = 0; i < n; i++ )
+            {
+                vet[i] = int.Parse(Console.ReadLine());
+            }
+
+            for ( int i = 0; i < vet.Length; i++ )
+            {
+                if ( vet[i] % 2 == 0 )
+                {
+                    listPar.Add(vet[i]);
+                }
+                else
+                {
+                    listImpar.Add(vet[i]);
+                }
+
+            }
+            listImpar.Sort();
+            listImpar.Reverse();
+            listPar.Sort();
+            var listaConcatenada = listPar.Concat(listImpar);
+
+            foreach ( var item in listaConcatenada )
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
