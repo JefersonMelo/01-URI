33c3f23c0b9ea471cf90ea47c60a5c1bbdad4792
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-06 16:29:53 -0300

    commit incial

---
+++ 01-Iniciante/URI_2554/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace URI_2554
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
