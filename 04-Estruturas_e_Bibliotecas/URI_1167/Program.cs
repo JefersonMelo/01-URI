1d0f26d631e22e57242c50d399e3dfa268b726dd
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-12 17:15:23 -0300

    commit inicial

---
+++ 04-Estruturas_e_Bibliotecas/URI_1167/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace URI_1167
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
