785aeb610d02ccf081586ef6f03a371b647c1a07
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-14 17:09:29 -0300

    commit readme e conclusão uri-1133

--- 01-Iniciante/URI_1133/Program.cs
+++ 01-Iniciante/URI_1133/Program.cs
@@ -4,37 +4,26 @@ namespace URI_1133
 {
     class Program
     {
-        /*
-        Escreva um programa que leia 2 valores X e Y e que imprima todos os valores entre eles cujo resto da divisão dele por 5 for igual a 2 ou igual a 3.
-        Entrada: O arquivo de entrada contém 2 valores positivos inteiros quaisquer, não necessariamente em ordem crescente.
-        Saída: Imprima todos os valores conforme exemplo abaixo, sempre em ordem crescente.
-        Sample Input 	Sample Output
-        10              12
-        18              13
-	                    17
-         */
-
-        static void Main( string[] args )
+        static void Main(string[] args)
         {
             int x, y, aux;
             x = int.Parse(Console.ReadLine());
             y = int.Parse(Console.ReadLine());
 
-            if (x < y)
+            if ( x < y )
             {
                 aux = y;
                 y = x;
                 x = aux;
             }
-            while (x > y)
+            for ( int i = (y + 1); i < x; i++ )
             {
-                
-                if (y % 5 == 2 || y % 5 == 3)
+                if ( i % 5 == 2 || i % 5 == 3 )
                 {
-                    Console.WriteLine(y);
+                    Console.WriteLine(i);
                 }
-                y++;
             }
         }
     }
 }
+
---
+++ 01-Iniciante/URI_1133/README.md
@@ -0,0 +1,20 @@
+# URI 1133
+
+## Resto da Divisão
+
+>URI Online Judge | 1133  
+>Adaptado por Neilor Tonin, URI Brasil :brazil:  
+
+Escreva um programa que leia 2 valores X e Y e que imprima todos os valores entre eles cujo resto da divisão dele por 5 for igual a 2 ou igual a 3.  
+
+## Entrada
+
+O arquivo de entrada contém 2 valores positivos inteiros quaisquer, não necessariamente em ordem crescente.  
+
+## Saída
+
+Imprima todos os valores conforme exemplo abaixo, sempre em ordem crescente.  
+
+|Sample Input|Sample Output|
+|-|-|
+|10<br>18|12<br>13<br>17|
