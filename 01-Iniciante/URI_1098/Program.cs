eace00d2426f12558446769af70645fb95894ec6
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-14 18:09:56 -0300

    commit readme e conclusão uri-1098

--- 01-Iniciante/URI_1098/Program.cs
+++ 01-Iniciante/URI_1098/Program.cs
@@ -5,40 +5,37 @@ namespace URI_1098
 {
     class Program
     {
-        /*
-        Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
-        Entrada: Não há nenhuma entrada neste problema.
-        Saída: Imprima a sequencia conforme exemplo abaixo.
-        Exemplo de Entrada 	Exemplo de Saída
-                            I=0 J=1
-                            I=0 J=2
-                            I=0 J=3
-                            I=0.2 J=1.2
-                            I=0.2 J=2.2
-                            I=0.2 J=3.2
-                            .....
-                            I=2 J=?
-                            I=2 J=?
-                            I=2 J=? 
-         */
-
-        static void Main( string[] args )
+        static void Main(string[] args)
         {
-            //nt j = 1;
-            for (double i = 0.0; i <= 2.0; i += .2)
-            {
+            double i, j, value;
+            int temp, temp2;
 
-                for (double j = 1; j <= 3; j++)
+            for ( i = 0, j = 1, value = 0, temp = 0, temp2 = 0; i < 2.1; j++ )
+            {
+                if ( temp2 == 0 )
                 {
-                    if (i % 2.0 == 0.0)
-                    {
-                        Console.WriteLine($"I = {i} J = {j + i}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"I = {i.ToString("F1", CultureInfo.InvariantCulture)} J = {(j + i).ToString("F1", CultureInfo.InvariantCulture)}");
-                    }
+                    Console.WriteLine($"I={i.ToString("F0", CultureInfo.InvariantCulture)} J={j.ToString("F0", CultureInfo.InvariantCulture)}");
                 }
+                else
+                {
+                    Console.WriteLine($"I={i.ToString("F1", CultureInfo.InvariantCulture)} J={j.ToString("F1", CultureInfo.InvariantCulture)}");
+                }
+
+                temp++;
+
+                if ( temp == 3 )
+                {
+                    i += 0.2;
+                    value += 0.2;
+                    j = value;
+                    temp = 0;
+                    temp2++;
+                }
+                if ( temp2 == 5 )
+                {
+                    temp2 = 0;
+                }
+
             }
         }
     }
---
+++ 01-Iniciante/URI_1098/README.md
@@ -0,0 +1,20 @@
+# URI 1098
+
+## Sequencia IJ 4
+
+>URI Online Judge | 1098  
+>Adaptado por Neilor Tonin, URI Brasil :brazil:  
+
+Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.  
+
+### Entrada
+
+Não há nenhuma entrada neste problema.  
+
+### Saída
+
+Imprima a sequencia conforme exemplo abaixo.  
+
+|Exemplo de Entrada|Exemplo de Saída|
+|-|-|
+||I=0 J=1<br>I=0 J=2<br>I=0 J=3<br>I=0.2 J=1.2<br>I=0.2 J=2.2<br>I=0.2 J=3.2<br>.....<br>I=2 J=?<br>I=2 J=?<br>I=2 J=?|
