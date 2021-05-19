b7a24804a2a5b548ed54833d561abea7b6f95d79
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-19 19:26:52 -0300

    commit com erro no retorno esperado: 86 anos. Entregue:  Mais de 1 seculo.

--- 01-Iniciante/URI_1160/Program.cs
+++ 01-Iniciante/URI_1160/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace URI_1160
 {
@@ -6,7 +7,46 @@ namespace URI_1160
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int t = Convert.ToInt32(Console.ReadLine());
+            int pa, pb;
+            double cpa, cpb;
+            int anos;
+
+            for ( int i = 0; i < t; i++ )
+            {
+                anos = 0;
+                string[] valores = Console.ReadLine().Split(' ');
+                pa = int.Parse(valores[0]);
+                pb = int.Parse(valores[1]);
+
+                //declare as variaveis corretamente
+                cpa = double.Parse(valores[2], CultureInfo.InvariantCulture);
+                cpb = double.Parse(valores[3], CultureInfo.InvariantCulture);
+
+                while ( pa <= pb )
+                {
+                    //complete o while
+                    pa += ( int )( pa * cpa ) / 100;
+                    pb += ( int )( pb * cpb ) / 100;
+                    anos++;
+
+                    if ( anos > 100 )
+                    {
+                        //complete a condicional
+                        break;
+                    }
+                }
+
+                if ( anos <= 100 )
+                {
+                    //complete a condicional
+                    Console.WriteLine($"{anos} anos.");
+                }
+                else
+                {
+                    Console.WriteLine("Mais de 1 seculo.");
+                }
+            }
         }
     }
 }
