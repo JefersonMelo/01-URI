9080157b789e556664a624535d8f7360ba8e8f38
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-19 21:08:54 -0300

    commit final, aceito.

--- 01-Iniciante/URI_1149/Program.cs
+++ 01-Iniciante/URI_1149/Program.cs
@@ -6,7 +6,28 @@ namespace URI_1149
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 0, n = 0, b = 0;
+
+            string[] line = Console.ReadLine().Split(' ');
+            x = int.Parse(line[0]);
+
+            for ( int i = 0; i < line.Length; i++ )
+            {
+                if ( int.Parse(line[i]) > 0 )
+                {
+                    n = int.Parse(line[i]);
+                }
+            }
+
+
+            while ( n <= 0 )
+                n = int.Parse(Console.ReadLine());
+            for ( int i = 1; i <= n; i++ )
+            {
+                b += x;
+                x++;
+            }
+            Console.WriteLine(b);
         }
     }
 }
