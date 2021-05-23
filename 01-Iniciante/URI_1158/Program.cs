991d398cf070a1be9bd79a4eceaff1a0689ba112
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-23 01:11:33 -0300

    commit conclusão uri 1158

--- 01-Iniciante/URI_1158/Program.cs
+++ 01-Iniciante/URI_1158/Program.cs
@@ -6,7 +6,28 @@ namespace URI_1158
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int x, y, somarImpar = 0;
+
+            while(n > 0)
+            {
+                string[] nums = Console.ReadLine().Split(' ');
+                x = int.Parse(nums[0]);
+                y = int.Parse(nums[1]);
+
+                while(y > 0)
+                {
+                    if(x % 2 != 0)
+                    {
+                        somarImpar += x;
+                        y--;
+                    }
+                    x++;
+                }
+                Console.WriteLine(somarImpar);
+                somarImpar = 0;
+                n--;
+            }
         }
     }
 }
