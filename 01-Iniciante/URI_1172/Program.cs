432d8e670715361b29872a61eeb5307aa2155bc1
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-23 21:05:51 -0300

    commit conclusão uri 1172

--- 01-Iniciante/URI_1172/Program.cs
+++ 01-Iniciante/URI_1172/Program.cs
@@ -6,7 +6,26 @@ namespace URI_1172
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] vet = new int[10];
+            int i;
+
+            for(i = 0; i < vet.Length; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+
+                if(x <= 0)
+                {
+                    vet[i] = 1;
+                }
+                else
+                {
+                    vet[i] = x;
+                }
+            }
+            for(i = 0; i < vet.Length; i++)
+            {
+                Console.WriteLine($"X[{i}] = {vet[i]}");
+            }
         }
     }
 }
