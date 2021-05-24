37b70f369a66d1efe6fd085d85fccb3aff3dc60f
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-24 19:36:11 -0300

    commit conclusão uri 1176

---
+++ 01-Iniciante/URI_1176/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace URI_1176
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double fibonacci = 1.0, seqFib = 0.0;
+            
+
+            for(int i = 0; i < n; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+
+                for(int j = 1; j < a; j++)
+                {
+                    fibonacci += seqFib;
+                    seqFib = fibonacci - seqFib;
+                }
+
+                if(a == 0) fibonacci = 0;
+
+                Console.WriteLine($"Fib({a}) = {fibonacci}");
+                fibonacci = 1.0;
+                seqFib = 0.0;
+            }
+        }
+    }
+}
