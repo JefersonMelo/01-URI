0183845ef694a9c5a0b06d9625fd190a4f732524
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-28 18:46:19 -0300

    commit ok uri 1040

--- 01-Iniciante/URI_1040/Program.cs
+++ 01-Iniciante/URI_1040/Program.cs
@@ -5,41 +5,12 @@ namespace URI_1040
 {
     class Program
     {
-        /*
-        Leia quatro números (N1, N2, N3, N4), cada um deles com uma casa decimal, correspondente às quatro notas de um aluno. 
-        Calcule a média com pesos 2, 3, 4 e 1, respectivamente, para cada uma destas notas e mostre esta média acompanhada pela 
-        mensagem "Media: ". Se esta média for maior ou igual a 7.0, imprima a mensagem "Aluno aprovado.". Se a média calculada for 
-        inferior a 5.0, imprima a mensagem "Aluno reprovado.". Se a média calculada for um valor entre 5.0 e 6.9, inclusive estas, 
-        o programa deve imprimir a mensagem "Aluno em exame.".
-        No caso do aluno estar em exame, leia um valor correspondente à nota do exame obtida pelo aluno. Imprima então a mensagem 
-        "Nota do exame: " acompanhada pela nota digitada. Recalcule a média (some a pontuação do exame com a média anteriormente 
-        calculada e divida por 2). e imprima a mensagem "Aluno aprovado." (caso a média final seja 5.0 ou mais ) ou "Aluno reprovado.", 
-        (caso a média tenha ficado 4.9 ou menos). Para estes dois casos (aprovado ou reprovado após ter pego exame) apresente na última 
-        linha uma mensagem "Media final: " seguido da média final para esse aluno.
-        Entrada: A entrada contém quatro números de ponto flutuante correspendentes as notas dos alunos.
-        Saída: Todas as respostas devem ser apresentadas com uma casa decimal. As mensagens devem ser impressas conforme a descrição do problema. 
-        Não esqueça de imprimir o enter após o final de cada linha, caso contrário obterá "Presentation Error".
-        Exemplo de Entrada 	Exemplo de Saída
-        2.0 4.0 7.5 8.0     Media: 5.4
-        6.4                 Aluno em exame.
-                            Nota do exame: 6.4
-                            Aluno aprovado.
-                            Media final: 5.9
-      
-        2.0 6.5 4.0 9.0     Media: 4.8
-                            Aluno reprovado.                 
-         */
-        
-        static void Main( string[] args )
+        static void Main(string[] args)
         {
-            //float a, b, c, d;
-            //float n5, media;
-            //int peso = 10;
-            //string[] s = Console.ReadLine().Split(' ');
-            double a = double.Parse(Console.ReadLine()); //float.Parse(s[0], CultureInfo.InvariantCulture);
-            double b = double.Parse(Console.ReadLine()); //float.Parse(s[1], CultureInfo.InvariantCulture);
-            double c = double.Parse(Console.ReadLine()); //float.Parse(s[2], CultureInfo.InvariantCulture);
-            double d = double.Parse(Console.ReadLine()); //float.Parse(s[3], CultureInfo.InvariantCulture);
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double d = double.Parse(Console.ReadLine());
 
             double media = ((a * 2) + (b * 3) + (c * 4) + (d * 1)) / 10;
             Console.WriteLine($"Media: {media.ToString("F1", CultureInfo.InvariantCulture)}");
