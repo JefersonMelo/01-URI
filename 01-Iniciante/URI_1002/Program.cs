9c91a753d3f56344e984ec262eba93d85af0505c
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-28 18:07:02 -0300

    commit com solução

--- 01-Iniciante/URI_1002/Program.cs
+++ 01-Iniciante/URI_1002/Program.cs
@@ -4,20 +4,7 @@ namespace URI_1002
 {
     class Program
     {
-        /*
-        A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
-        - Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
-        Entrada: A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
-        Saída: Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal. 
-        Utilize variáveis de dupla precisão (double). Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, 
-        caso contrário, você receberá "Presentation Error".
-        Exemplos de Entrada 	Exemplos de Saída
-        2.00                    A=12.5664
-	
-        100.64                  A=31819.3103
-        */
-
-        static void Main( string[] args )
+        static void Main(string[] args)
         {
             double N = 3.14159;
             double A, R;
