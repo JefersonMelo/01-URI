a720a722ab5973aa2b54455055d23a2160df3d1b
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-30 11:55:00 -0300

    commit uri 1045

--- 01-Iniciante/URI_1045/Program.cs
+++ 01-Iniciante/URI_1045/Program.cs
@@ -4,26 +4,7 @@ namespace URI_1045
 {
     class Program
     {
-        /*
-        Leia 3 valores de ponto flutuante A, B e C e ordene-os em ordem decrescente, de modo que o lado A representa o maior dos 3 lados. 
-        A seguir, determine o tipo de triângulo que estes três lados formam, com base nos seguintes casos, sempre escrevendo uma mensagem adequada:
-            se A ≥ B+C, apresente a mensagem: NAO FORMA TRIANGULO
-            se A2 = B2 + C2, apresente a mensagem: TRIANGULO RETANGULO
-            se A2 > B2 + C2, apresente a mensagem: TRIANGULO OBTUSANGULO
-            se A2 < B2 + C2, apresente a mensagem: TRIANGULO ACUTANGULO
-            se os três lados forem iguais, apresente a mensagem: TRIANGULO EQUILATERO
-            se apenas dois dos lados forem iguais, apresente a mensagem: TRIANGULO ISOSCELES
-        Entrada: A entrada contem três valores de ponto flutuante de dupla precisão A (0 < A) , B (0 < B) e C (0 < C).
-        Saída: Imprima todas as classificações do triângulo especificado na entrada.
-        Exemplos de Entrada 	Exemplos de Saída
-        7.0 5.0 7.0             TRIANGULO ACUTANGULO
-                                TRIANGULO ISOSCELES
-	
-        6.0 6.0 10.0            TRIANGULO OBTUSANGULO
-                                TRIANGULO ISOSCELES 
-         */
-
-        static void Main( string[] args )
+        static void Main(string[] args)
         {
             string[] s = Console.ReadLine().Split(' ');
             double a, b, c, aux;
@@ -63,7 +44,6 @@ namespace URI_1045
                     b = aux;
                 }
             }
-            //Console.WriteLine($"A:{a} B:{b} C:{c}\n");
             if (a >= (b + c)) { Console.WriteLine("NAO FORMA TRIANGULO"); }
             else if (a * a == (b * b + c * c)) { Console.WriteLine("TRIANGULO RETANGULO"); }
             else if (a * a > (b * b + c * c)) { Console.WriteLine("TRIANGULO OBTUSANGULO"); }
