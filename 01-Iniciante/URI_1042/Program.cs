7c03653aae1d01f92356dd1895c8c42b70825ac0
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-30 11:49:39 -0300

    commit ok uri 1042

--- 01-Iniciante/URI_1042/Program.cs
+++ 01-Iniciante/URI_1042/Program.cs
@@ -4,30 +4,7 @@ namespace URI_1042
 {
     class Program
     {
-        /*
-        Leia 3 valores inteiros e ordene-os em ordem crescente. No final, mostre os valores em ordem crescente, 
-        uma linha em branco e em seguida, os valores na sequência como foram lidos.
-        Entrada: A entrada contem três números inteiros.
-        Saída: Imprima a saída conforme foi especificado.
-        Exemplo de Entrada 	Exemplo de Saída
-        7 21 -14            -14
-                            7
-                            21
-
-                            7
-                            21
-                            -14
-	
-        -14 21 7            -14
-                            7
-                            21
-
-                            -14
-                            21
-                            7
-         */
-
-        static void Main( string[] args )
+        static void Main(string[] args)
         {
             string[] s = Console.ReadLine().Split(' ');
             int a, b, c;
