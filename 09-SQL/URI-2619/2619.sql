35d24f6cac1a794a062c45cb871f096f9e38ebcd
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-30 11:41:34 -0300

    commit ok uri 2619

--- 09-SQL/URI-2619/2619.sql
+++ 09-SQL/URI-2619/2619.sql
@@ -56,6 +56,8 @@ VALUES
 
 SELECT products.name, providers.name, products.price
 FROM products
-JOIN providers ON providers.id = products.id_providers
-JOIN categories ON categories.id = products.id_categories
+JOIN providers 
+ON providers.id = products.id_providers
+JOIN categories 
+ON categories.id = products.id_categories
 WHERE products.price > 1000 AND categories.name LIKE 'Super Luxury';
