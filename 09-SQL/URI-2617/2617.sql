7772ad597d7549d229240d3143cd8f0321a1de37
Jeferson Melo <jefsdemelo@gmail.com>
2021-05-30 11:38:19 -0300

    commit ok uri 2617

--- 09-SQL/URI-2617/2617.sql
+++ 09-SQL/URI-2617/2617.sql
@@ -42,5 +42,6 @@ VALUES
 
 SELECT products.name, providers.name
 FROM products
-INNER JOIN providers ON products.id_providers = providers.id
+INNER JOIN providers 
+ON products.id_providers = providers.id
 WHERE providers.name = 'Ajax SA';
