1385b4eb503e460638ce832b8c4de3f3b4523de9
Jeferson Melo <jefsdemelo@gmail.com>
2021-06-05 17:51:48 -0300

    commit ok uri 1065

--- 01-Iniciante/URI_1065/Program.cs
+++ 01-Iniciante/URI_1065/Program.cs
@@ -4,19 +4,7 @@ namespace URI_1065
 {
     class Program
     {
-        /*
-        Faça um programa que leia 5 valores inteiros. Conte quantos destes valores digitados são pares e mostre esta informação.
-        Entrada: O arquivo de entrada contém 5 valores inteiros quaisquer.
-        Saída: Imprima a mensagem conforme o exemplo fornecido, indicando a quantidade de valores pares lidos.
-        Exemplo de Entrada 	Exemplo de Saída
-        7                   3 valores pares
-        -5
-        6
-        -4
-        12
-         */
-
-        static void Main( string[] args )
+        static void Main(string[] args)
         {
             int[] array = new int[5];
             int cont = 0;
