5568dd83ebb6806386d5e24179a96635637c2f5c
Jeferson Melo <jefsdemelo@gmail.com>
2021-06-06 12:46:26 -0300

    commit ok uri 1548

--- 04-Estruturas_e_Bibliotecas/READMD.md
+++ 04-Estruturas_e_Bibliotecas/READMD.md
@@ -3,5 +3,7 @@
 | ID   | Problema                  | Status |
 | ---- | ------------------------- | ------ |
 |1167|[Acampamento de Férias](https://github.com/JefersonMelo/01-URI/blob/master/04-Estruturas_e_Bibliotecas/URI_1167/Program.cs)|:x:|
+|1258|[Camisetas](https://github.com/JefersonMelo/01-URI/blob/master/04-Estruturas_e_Bibliotecas/URI_1258/Program.cs)|:x:|
 |1259|[Pares e Ímpares](https://github.com/JefersonMelo/01-URI/blob/master/04-Estruturas_e_Bibliotecas/URI_1259/Program.cs)|:white_check_mark:|
 |1548|[Pares e Ímpares](https://github.com/JefersonMelo/01-URI/blob/master/04-Estruturas_e_Bibliotecas/URI_1548/Program.cs)|:white_check_mark:|
+|2136|[Pares e Ímpares](https://github.com/JefersonMelo/01-URI/blob/master/04-Estruturas_e_Bibliotecas/URI_1548/Program.cs)||
--- 04-Estruturas_e_Bibliotecas/URI_1548/Program.cs
+++ 04-Estruturas_e_Bibliotecas/URI_1548/Program.cs
@@ -5,11 +5,11 @@ namespace URI_1548
 {
     class Program
     {
-        /*
-        https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1.reverse?redirectedfrom=MSDN&view=net-5.0#System_Collections_Generic_List_1_Reverse
-        https://social.msdn.microsoft.com/Forums/pt-BR/a19997f5-47b8-4732-84c2-74ed8a2468e9/ordem-decrescente?forum=vscsharppt
-        https://www.devmedia.com.br/forum/3-numeros-em-ordem-crescente-csharp/597322
-         */
+    /*
+    https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1.reverse?redirectedfrom=MSDN&view=net-5.0#System_Collections_Generic_List_1_Reverse
+    https://social.msdn.microsoft.com/Forums/pt-BR/a19997f5-47b8-4732-84c2-74ed8a2468e9/ordem-decrescente?forum=vscsharppt
+    https://www.devmedia.com.br/forum/3-numeros-em-ordem-crescente-csharp/597322
+     */
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
@@ -18,23 +18,23 @@ namespace URI_1548
             int numReordem = 0;
             List<int> list = new List<int>();
 
-            for ( int i = 0; i < n; i++ )
+            for (int i = 0; i < n; i++)
             {
                 qtdEntradas = int.Parse(Console.ReadLine());
                 string[] s = Console.ReadLine().Split(' ');
                 notas = new int[qtdEntradas];
-                for ( int j = 0; j < qtdEntradas; j++ )
+                for (int j = 0; j < qtdEntradas; j++)
                 {
                     notas[j] = int.Parse(s[j]);
                     list.Add(notas[j]);
 
-                    if ( j == ( qtdEntradas - 1 ) )
+                    if (j == (qtdEntradas - 1))
                     {
                         list.Sort();
                         list.Reverse();
-                        for ( int k = 0; k < list.Count; k++ )
+                        for (int k = 0; k < list.Count; k++)
                         {
-                            if ( notas[k] == list[k] )
+                            if (notas[k] == list[k])
                             {
                                 numReordem++;
                             }
