e53ee3c96c2c50bc66d8a5d949c8f7afedf09eb3
Jeferson Melo <jefsdemelo@gmail.com>
2021-06-27 18:26:41 -0300

    commit ide ok. erro no uri

--- 04-Estruturas_e_Bibliotecas/URI_1258/Program.cs
+++ 04-Estruturas_e_Bibliotecas/URI_1258/Program.cs
@@ -4,22 +4,6 @@ using System.Linq;
 
 namespace URI_1258
 {
-    /*
-     * https://urisolve.blogspot.com/2017/04/uri-solution-1258-t-shirts-in-c.html
-     * 
-     * https://www.w3schools.com/tryit/tryit.asp?filename=tryhtml_default
-     * 
-     * https://pt.stackoverflow.com/questions/5083/como-ordenar-lista-com-objeto-complexo-por-uma-de-suas-propriedades
-     * 
-     * https://docs.microsoft.com/pt-br/dotnet/api/system.collections.generic.list-1.sort?view=net-5.0
-     * 
-     * https://www.delftstack.com/pt/howto/csharp/how-to-sort-a-list-by-a-property-in-the-object-in-csharp/#:~:text=Usando%20o%20delegado-,Utilize%20o%20m%C3%A9todo%20OrderBy%20para%20ordenar%20uma%20lista%20por%20uma,ordenada%20pelo%20bem%20em%20quest%C3%A3o.
-     * 
-     * https://csharp.net-tutorials.com/pt/439/linq/organizando-dados-os-metodos-orderby-thenby/
-     * 
-     * https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/concepts/linq/sorting-data
-     * 
-     */
     class Program
     {
         static void Main(string[] args)
