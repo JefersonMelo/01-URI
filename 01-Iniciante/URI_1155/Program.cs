5b56a896472e58a099c576cdf13314d7595110c7
Jeferson Melo <jefsdemelo@gmail.com>
2021-08-05 14:47:32 -0300

    conclusão uri 1155

--- 01-Iniciante/URI_1155/Program.cs
+++ 01-Iniciante/URI_1155/Program.cs
@@ -2,16 +2,16 @@
 
 namespace URI_1155
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            float s = 0;
-            for ( int i = 1; i <= 100; i++ )
-            {
-                s += ( ( float )1 / ( float )i );
-            }
-            Console.WriteLine($"{s:F2}");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			float s = 0;
+			for(int i = 1; i <= 100; i++)
+			{
+				s += ((float)1 / (float)i);
+			}
+			Console.WriteLine($"{s:F2}");
+		}
+	}
 }
