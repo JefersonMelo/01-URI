4b06c9c25d9928719c993433619d5237fb7e8255
Jeferson Melo <jefsdemelo@gmail.com>
2021-08-05 14:44:30 -0300

    conclusão uri 1172

--- 01-Iniciante/URI_1172/Program.cs
+++ 01-Iniciante/URI_1172/Program.cs
@@ -2,30 +2,30 @@
 
 namespace URI_1172
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] vet = new int[10];
-            int i;
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] vet = new int[10];
+			int i;
 
-            for(i = 0; i < vet.Length; i++)
-            {
-                int x = int.Parse(Console.ReadLine());
+			for(i = 0; i < vet.Length; i++)
+			{
+				int x = int.Parse(Console.ReadLine());
 
-                if(x <= 0)
-                {
-                    vet[i] = 1;
-                }
-                else
-                {
-                    vet[i] = x;
-                }
-            }
-            for(i = 0; i < vet.Length; i++)
-            {
-                Console.WriteLine($"X[{i}] = {vet[i]}");
-            }
-        }
-    }
+				if(x <= 0)
+				{
+					vet[i] = 1;
+				}
+				else
+				{
+					vet[i] = x;
+				}
+			}
+			for(i = 0; i < vet.Length; i++)
+			{
+				Console.WriteLine($"X[{i}] = {vet[i]}");
+			}
+		}
+	}
 }
